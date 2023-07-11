9e7eab8d62ff1b66022e4e34b9b7588895acc446
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-11 23:34:57 +0300

    Inject Quill Inline

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,36 @@
 import './style.css'
-import { onDragStart, onDragEnd, onDragOver, onDrop, onClick, onClear, onSave, onRestore } from './dnd.ts'
+import { onDragStart, onDragEnd, onDragOver, onDrop, onClick, onClear, onSave, onRestore, setupGlobalEvents } from './dnd.ts'
 
 let builderContainer = document.querySelector('#layout')!.innerHTML;
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = builderContainer;
 
 // SETUP Navigation
-document.querySelector('#action_clear')!.addEventListener('click'   , (event) => { onClear   ( event ) });
-document.querySelector('#action_save')!.addEventListener('click'    , (event) => { onSave    ( event ) });
-document.querySelector('#action_restore')!.addEventListener('click' , (event) => { onRestore ( event ) });
-document.querySelector('#action_undo')!.addEventListener('click'    , (event) => { onRestore ( event ) });
+document.querySelector('#action_clear')!.addEventListener('click', (event) => { onClear(event) });
+document.querySelector('#action_save')!.addEventListener('click', (event) => { onSave(event) });
+document.querySelector('#action_restore')!.addEventListener('click', (event) => { onRestore(event) });
+document.querySelector('#action_undo')!.addEventListener('click', (event) => { onRestore(event) });
 
 // SETUP Components
 let draggableElems = document.getElementsByClassName('draggable');
 
 for (let i = 0; i < draggableElems.length; i++) {
-    draggableElems[i].addEventListener('dragstart', (event) => { onDragStart( event ) });
-    draggableElems[i].addEventListener('dragend'  , (event) => { onDragEnd  ( event ) });
-    draggableElems[i].addEventListener('click'    , (event) => { onClick    ( event ) });
-} 
+    draggableElems[i].addEventListener('dragstart', (event) => { onDragStart(event) });
+    draggableElems[i].addEventListener('dragend', (event) => { onDragEnd(event) });
+}
 
 // SETUP Master DROP Zone
-document.querySelector('#dropzone')!.addEventListener('dragover', (event) => { onDragOver( event ) });
-document.querySelector('#dropzone')!.addEventListener('drop'    , (event) => { onDrop    ( event ) });
+document.querySelector('#dropzone')!.addEventListener('dragover', (event) => { onDragOver(event) });
+document.querySelector('#dropzone')!.addEventListener('drop', (event) => { onDrop(event) });
+document.querySelector('#dropzone')!.addEventListener('click', (event) => { onClick(event) });
 
 // SETUP GRID Drop Zones
 let dropZones = document.getElementsByClassName('dropzone-elem');
 for (let i = 0; i < dropZones.length; i++) {
-    dropZones[i].addEventListener('dragover', (event) => { onDragOver( event ) });
-    dropZones[i].addEventListener('dragend' , (event) => { onDragEnd ( event ) });
-    dropZones[i].addEventListener('drop'    , (event) => { onDrop    ( event ) });
+    dropZones[i].addEventListener('dragover', (event) => { onDragOver(event) });
+    dropZones[i].addEventListener('dragend', (event) => { onDragEnd(event) });
+    dropZones[i].addEventListener('drop', (event) => { onDrop(event) });
 }
 
-onRestore( null );
+onRestore(null);
 
-
-// Initialize Quill editor
-var options = {
-    theme: 'snow',
-};
-new Quill('#editor', options);
+setupGlobalEvents();
