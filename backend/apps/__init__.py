a89c7f820b5b2a7f1b14894d029557a9d2b8882c
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-12 21:25:11 +0300

    BACKEND - Enable CORS

--- backend/apps/__init__.py
+++ backend/apps/__init__.py
@@ -7,11 +7,13 @@ import os
 
 # import Flask 
 from flask import Flask
+from flask_cors import CORS
 
 from .config import Config
 
 # Inject Flask magic
 app = Flask(__name__)
+CORS(app)
 
 # load Configuration
 app.config.from_object( Config ) 
